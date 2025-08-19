256cf7ed930891e042fd849fee006002c4b6947a
kodar@siri <shirishakodari35@.com>
2025-08-19 12:56:59 +0530

    jenkins

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "jenkins" {
   ami           = local.ami_id
   instance_type = "t3.small"
   vpc_security_group_ids = [aws_security_group.main.id]
-  subnet_id = "subnet-0ea509ad4cba242d7" #replace your Subnet
+  subnet_id = "subnet-084475d91bc25bbc3" #replace your Subnet
 
   # need more for terraform
   root_block_device {
