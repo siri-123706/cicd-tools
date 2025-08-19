28ee758de1dfd5c83a41f0bbf362710590a63e9a
kodar@siri <shirishakodari35@.com>
2025-08-19 12:46:17 +0530

    jenkins

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "daws84s.cfd"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z0673912SPW6GV3I8NW2"
+  description = "description"
+}
