60dbe5a4bbefc7f146298f05d5483bfd610a4c45
shirishakodari <kodarishirisha419@gmail.com>
2025-08-23 13:59:50 +0530

    jenkins

--- jenkins-agent.sh
+++ jenkins-agent.sh
@@ -27,7 +27,7 @@ yum install zip -y
 yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-systemctl start docker
+systemctl start docker 
 systemctl enable docker
 usermod -aG docker ec2-user
 
